788d73d20be1285fd1d22bf822f786d3e9d3de42
Ethan Plunkett <ethan@plunkettworks.com>
2023-01-10 12:46:25 -0500

    added crs(), ext(), res(), xres(), and yres() methods for terra generics

--- R/dimensions.R
+++ R/dimensions.R
@@ -1,14 +1,21 @@
 #' @rdname dimensions
-#' @name dimensions
+#' @name BirdFlow dimensions
 #' @title Dimensions of a BirdFlow object
 #' @description
-#' Functions to return BirdFlow model dimensions
-#'
+#' Functions to return BirdFlow model dimensions and other basic information
+#' @importFrom terra crs ext res xres yres
+#' @importMethodsFrom terra crs ext res xres yres
 #' @aliases nrow ncol dim ext
 #' @param x A BirdFlow object
-#'
+#' @inheritParams terra::crs
 #' @return `nrow()` returns the number of rows in the raster extent of the BirdFlow model
 #' @method nrow BirdFlow
+#' @seealso
+#' \pkg{terra} defines the S4 generics for [crs()][terra::crs()],
+#' [ext()][terra::ext()], [res()][terra::res()], [xres()][terra::xres()], and
+#' [yres()][terra::yres()]
+#'
+#' [get_distr()] returns distributions from a `BirdFlow` object.
 #' @export
 nrow.BirdFlow <- function(x){
   x$geom$nrow
@@ -17,7 +24,7 @@ nrow.BirdFlow <- function(x){
 #' @rdname dimensions
 #' @method ncol BirdFlow
 #' @export
-#' @return `ncol()` returns the number of columns in the raster extent of a BirdFlow model
+#' @return `ncol()` number of columns in the raster extent
 ncol.BirdFlow <- function(x){
   x$geom$ncol
 }
@@ -25,32 +32,82 @@ ncol.BirdFlow <- function(x){
 #' @rdname dimensions
 #' @method dim BirdFlow
 #' @export
-#' @return `dim()` returns the number of rows and columns in the raster extent of a BirdFlow model
+#' @return `dim()` number of rows and columns in the raster extent.
 dim.BirdFlow <- function(x){
   c(x$geom$nrow, x$geom$ncol)
 }
 
 #' @rdname dimensions
 #' @export
-#' @return `n_timesteps()` returns the number of timesteps in a BirdFlow model
+#' @return `n_timesteps()` number of timesteps, which is the same as the number of
+#'  distributions.
 n_timesteps <- function(x){
   x$n_timesteps
 }
 
 #' @rdname dimensions
 #' @export
-#' @return `n_transitions()` returns the number of transitions in a BirdFlow model
+#' @return `n_transitions()` number of transitions, if the model is circular in
+#' time this will equal `n_timesteps()`.
 n_transitions <- function(x){
   x$n_trans
 }
 
 #' @rdname dimensions
 #' @export
-#' @return `n_active()` returns the number of active cells (locations)
-#' in a BirdFlow model.
+#' @return `n_active()` number of active cells (locations).
 n_active <- function(x){
- x$n_active
+  x$n_active
 }
+
+
+# The section below defines BirdFlow methods for S4 generic functions from
+# terra it allows
+
+#  setOldClass("BirdFlow") allows S4 dispatch on S3 BirdFlow objects.
+setOldClass("BirdFlow")
+
+crs.BirdFlow <- function(x, proj=FALSE, describe=FALSE, parse=FALSE){
+  terra::crs(x$geom$crs, proj, describe, parse)
+}
+#' @rdname dimensions
+#' @export
+#' @return `crs()` the coordinate reference system, a character with well known
+#' text (wkt) by default, but see arguments.
+setMethod(crs, "BirdFlow", crs.BirdFlow)
+
+ext.BirdFlow <- function(x){
+  terra::ext(bf$geom$ext)
+}
+#' @rdname dimensions
+#' @export
+#' @return `ext()` a [SpatExtent][terra::SpatExtent] object,
+#' which contains the xmin, xmax, ymin, and ymax of the extent.
+setMethod(ext, "BirdFlow", ext.BirdFlow)
+
+res.BirdFlow <- function(x){
+  bf$geom$res
+}
+#' @rdname dimensions
+#' @return `res()` two numbers, the cell width and height (x and y resolution).
+#' @export
+setMethod(res, "BirdFlow", res.BirdFlow)
+
+xres.BirdFlow <- function(x){
+  bf$geom$res[1]
+}
+#' @rdname dimensions
+#' @return `xres()` the width of the cells (x resolution).
+#' @export
+setMethod(xres, "BirdFlow", xres.BirdFlow)
+
+yres.BirdFlow <- function(x){
+  bf$geom$res[2]
+}
+#' @rdname dimensions
+#' @return `yres()` the height of the cells (y resolution).
+#' @export
+setMethod(yres, "BirdFlow", yres.BirdFlow)
 
 
 
