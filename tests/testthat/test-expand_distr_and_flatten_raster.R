4e6606793a95b34f41efda8de29b36f739bd9d2a
Ethan Plunkett <ethan@plunkettworks.com>
2023-02-28 23:38:04 -0500

    renamed collapse_distr() to flatten_raster(raster, bf)

--- tests/testthat/test-expand_distr_and_flatten_raster.R
+++ tests/testthat/test-expand_distr_and_flatten_raster.R
@@ -8,19 +8,28 @@
 #------------------------------------------------------------------------------#
 
 
-test_that("expand and collapse distr are reversable", {
+test_that("expand_distr and flatten_raster are reversable - 1 distr", {
   bf <- make_test_birdflow()
   d <- get_distr(1, bf)
-  e <- expand_distr(d, bf)
-  f <- collapse_distr(e, bf)
+  r <- expand_distr(d, bf)
+  f <- flatten_raster(r, bf)
   expect_equal(d, f)
 })
 
-test_that("collapse_distr is consistent with i_to_rc() subset", {
+test_that("expand_distr and flatten_raster are reversable - multiple distr", {
+  bf <- BirdFlowModels::amewoo
+  d <- get_distr(3:4, bf)
+  r <- expand_distr(d, bf)
+  f <- flatten_raster(r, bf)
+  expect_equal(d, f)
+})
+
+
+test_that("flatten_raster is consistent with i_to_rc() subset", {
   bf <- make_test_birdflow()
   d <- runif(n = bf$n_active)
   m <- matrix(NA, nrow = bf$geom$nrow, ncol = bf$geom$ncol)
   m[i_to_rc(1:bf$n_active, bf)] <- d
-  d2 <- collapse_distr(m, bf)
+  d2 <- flatten_raster(m, bf)
   expect_equal(d, d2)
 })
