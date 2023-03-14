87bd2eccdd531bb1428278761c1e09bbb0916691
Ethan Plunkett <ethan@plunkettworks.com>
2023-03-13 22:05:06 -0400

    Fixed CRAN check failure caused by changes in SF. (#40)
    * Fixed CRAN check failure caused by changes in SF.  Updated news for #38 and #37

--- tests/testthat/test-route_migration.R
+++ tests/testthat/test-route_migration.R
@@ -4,7 +4,11 @@ test_that("route_migration() works with n = 1", {
   expect_error( rts <- route_migration(BirdFlowModels::amewoo, n = 1), NA )
 
   # Expect consistent output
-  expect_snapshot(rts)
+  novel_points <- rts$points[!duplicated(rts$points[, c("x", "y", "route")]), ]
+  # Expect consistent output
+  expect_snapshot(novel_points)
+  expect_equal(class(rts$lines), c("sf", "data.frame"))
+  expect_equal(length(rts$lines), 2)
 
 })
 
