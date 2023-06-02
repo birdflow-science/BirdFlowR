449e0574d8262ff47ecc7c61e367a0e94aa8b3cc
Ethan Plunkett <ethan@plunkettworks.com>
2023-06-02 16:01:01 -0400

    Added tests for add_dynamic_mask()
    Fixes #101

---
+++ tests/testthat/test-add_dynamic_mask.R
@@ -0,0 +1,9 @@
+test_that("Add dynamic mask works", {
+
+          bf <- BirdFlowModels::amewoo
+          expect_no_error(bf <- add_dynamic_mask(bf))
+          expect_no_error(validate_BirdFlow(bf))
+          expect_no_error(dbf <- add_dynamic_mask(bf, dummy_mask = TRUE))
+          expect_no_error(validate_BirdFlow(dbf))
+
+         } )
