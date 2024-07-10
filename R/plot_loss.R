8df7325a7b1503242ca04c8bb04ed39b83f3d9bc
Ethan Plunkett <ethan@plunkettworks.com>
2024-07-10 17:13:55 +0000

    add plot_loss() function

---
+++ R/plot_loss.R
@@ -0,0 +1,52 @@
+#' Plot changes in component and total loss during model fitting
+#'
+#' Model fitting  - in [BirdFlowPy]() - attempts to minimize the total weighted
+#' loss which has three components.
+#'
+#' * Observation loss captures how well the model predicts the Status and
+#' Trend distributions it was trained on.  Its weight is always set to 1 and its
+#' relative weight is changed by adjusting the other to weights which are
+#' usually much less than 1.
+#' * Distance loss is lower when the routes encoded in the model are shorter.
+#' * Entropy loss is lower when the entropy in the model is higher.
+#'
+#' The total loss is the weighted sum of the three component losses. The
+#' weighting may cause it to be lower than some of the components.
+#'
+#' @param bf A fitted BirdFlowModel
+#' @param transform Passed to [ggplot2::scale_y_continuous()] to set the y-axis
+#' transformation. Reasonable values for this function include
+#' "identity", "log", "log10", "log2", and "sqrt".
+#' @return a **ggplot2** plot object.
+#' @export
+#' @examples
+#'  bf <- BirdFlowModels::amewoo
+#'  plot_loss(bf)
+plot_loss <- function(bf, transform = "log10") {
+
+  if(!all(c("loss_values", "hyperparameters") %in% names(bf$metadata)))
+    stop("Missing loss or hyperparmers from metadata. Is this a fitted model?")
+  loss <- bf$metadata$loss_values |>
+    dplyr::rename(Distance = dist,
+                  Entropy = ent,
+                  Observation = obs,
+                  Total = total)
+  loss$Step <- seq_len(nrow(loss))
+
+
+  # Convert to long format
+  d <- tidyr::pivot_longer(loss, cols = -ncol(loss), values_to = "Loss", names_to = "Type")
+
+  hp <-  bf$metadata$hyperparameters
+
+  subtitle <- paste0("Weights: Observation:", hp$obs_weight,
+                     " Distance: ", hp$dist_weight,
+                     " Entropy: ", hp$ent_weight)
+
+  ggplot(data = d, aes(x = Step, y = Loss, color = Type)) +
+    ggplot2::geom_line(linewidth = .8) +
+    ggplot2::scale_y_continuous(transform = "log10") +
+    ggplot2::ggtitle(paste0(species(bf), " Loss"), subtitle = subtitle)
+
+
+}
