d4d3bde08e8aab82afbdfa5fe53d381f7726beab
chenyangkang <chenyangkang24@outlook.com>
2025-01-17 23:25:46 +0000

    add initial tests for RoutesDataClass -- More to add

---
+++ tests/testthat/helper-fake_routes.R
@@ -0,0 +1,17 @@
+#' Make fake routes
+#'
+#' `make_fake_routes()` is a helper function used in testing `Routes`, `BirdFlowRoutes`, and `BirdFlowIntervals`.
+#'
+#' @return a dataframe with routes info
+
+make_fake_routes <- function(){
+  route_df1 <- data.frame(
+    route_id = c("001", "001", "001", "001", "001", "003", "003", "003", "004"),
+    date = as.Date(c("2025-01-01", "2025-01-08", "2025-01-15", "2025-01-21", "2025-02-10", "2025-03-01", "2025-05-01", "2025-06-01", "2025-05-01")),
+    lon = c(-75.0060, -75.0060, -74.0060, -87.6298, -87.6298, -87.6298, -89.6298, -85.6298, -95.3698),
+    lat = c(39.7128, 39.7128, 40.7128, 41.8781, 41.8781, 41.8781, 42.8781, 40.8781, 29.7604),
+    route_type = c("tracking", 'tracking', "tracking", 'tracking', 'tracking', "motus", "motus", "motus", "motus")
+  )
+  return(route_df1)
+}
+
---
+++ tests/testthat/test-RouteDataClass.R
@@ -0,0 +1,12 @@
+test_that("Routes() -> as_BirdFlowRoutes() -> as_BirdFlowIntervals() works", {
+    fake_routes <- make_fake_routes()
+    bf <- BirdFlowModels::amewoo
+    species1 = 'aa'
+    source1 = list(a=c('1'), b=c('2'))
+
+    expect_no_error(my_routes <- Routes(fake_routes, species1, source1))
+    expect_no_error(my_bfroutes <- as_BirdFlowRoutes(my_routes, bf=bf))
+    expect_no_error(my_intervals <- as_BirdFlowIntervals(my_bfroutes))
+})
+
+## More to add -- print, etc
