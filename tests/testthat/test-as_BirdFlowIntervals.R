fb8fd438e5bdd15e2f0c77beda08430b19705a89
Ethan Plunkett <ethan@plunkettworks.com>
2025-03-28 16:01:04 -0400

    Move as_BirdFlowRoutes test into its own file

--- tests/testthat/test-as_BirdFlowIntervals.R
+++ tests/testthat/test-as_BirdFlowIntervals.R
@@ -89,8 +89,6 @@ test_that("Test Interval sampling strategy", {
 })
 
 
-
-## More to add -- print, etc
 test_that("If no intervals can be sampled, return NULL", {
   set.seed(42)
   fake_routes <- make_fake_routes_one_point_per_route()
@@ -104,87 +102,6 @@ test_that("If no intervals can be sampled, return NULL", {
   expect_no_error(my_intervals <- as_BirdFlowIntervals(my_bfroutes))
   expect_null(my_intervals)
 })
-
-
-
-
-test_that("Routes() -> as_BirdFlowRoutes() with different aggregations works", {
-  set.seed(42)
-  bf <- BirdFlowModels::amewoo
-
-  fake_tracks <- make_fake_tracking_data(bf, 5,
-                                         start = 10,
-                                         end = 15,
-                                         interval =
-                                           as.difftime(.2, units = "days"),
-                                         bandwidth = 20,
-                                         year_sd = 0)
-  species1 <- "amewoo"
-  source1 <- "eBird"
-  expect_no_error(
-    my_routes <- Routes(fake_tracks, species = species1, source = source1))
-
-  expect_equal(nrow(fake_tracks), nrow(my_routes$data))
-
-
-  data <- my_routes$data
-  data$timestep <- lookup_timestep(data$date, bf = bf)
-  data <- cbind(data,
-                latlon_to_xy(data[, c("lat", "lon")], bf = bf))
-
-  data$is_valid <- is_location_valid(bf, x = data$x, y = data$y,
-                                     date = data$date)
-  cols <- c("route_id", "x", "y", "date")
-
-  # Mean
-  expect_no_error(my_bfroutes <- as_BirdFlowRoutes(my_routes, bf=bf,
-                                                   aggregate='mean'))
-
-
-  means <- data |>
-    dplyr::group_by(.data$route_id, .data$timestep) |>
-    dplyr::summarize(x = mean(x),
-              y = mean(y),
-              date = mean(date)) |>
-    as.data.frame()
-
-  expect_equal(my_bfroutes$data[, cols], means[, cols])
-
-
-  # Median
-  expect_no_error(my_bfroutes <- as_BirdFlowRoutes(my_routes, bf=bf,
-                                                   aggregate = "median"))
-
-  medians <- my_bfroutes$data |>
-    dplyr::group_by(.data$route_id, .data$timestep) |>
-    dplyr::summarize(x = median(x),
-                     y = median(y),
-                     date = median(date)) |>
-    as.data.frame()
-  expect_equal(my_bfroutes$data[, cols], medians[, cols])
-
-
-  # Midweek
-  expect_no_error(my_bfroutes <- as_BirdFlowRoutes(my_routes, bf=bf,
-                                                   aggregate='midweek'))
-
-  # Multiple observations per day so midweek observation should always be
-  # on the midweek day
-  result_dates <- my_bfroutes$data$date |> lubridate::as_date()
-  expected_dates <- my_bfroutes$data$timestep |> lookup_date(bf = bf)
-  expect_equal(result_dates, expected_dates)
-
-
-
-  set.seed(1)
-  expect_no_error(my_bfroutes <- as_BirdFlowRoutes(my_routes, bf=bf,
-                                                   aggregate='random'))
-
-  expect_snapshot(my_bfroutes$data[1:10, c("route_id", "i", "timestep")])
-
-
-})
-
 
 
 
