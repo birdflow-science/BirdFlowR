59335f0d96aab0aa87fc832e380133c09fdf7c72
Atharva Shahane <atharvashahane28@gmail.com>
2026-02-08 12:42:05 -0500

    feat: more tests for rasterize_bmtr

--- tests/testthat/test-rasterize_bmtr.R
+++ tests/testthat/test-rasterize_bmtr.R
@@ -1,9 +1,3 @@
-# Todo
-# Testing:
-# Bare min: Create a bmtr and rasterize it **without an error**
-# PBT 1: expect_equal(ext(bf), ext(bmtr_raster)) -> ext is the extent function
-# PBT 2: number of layers in raster (layers function) == number of timesteps -> (loose equality constraint, allow some tolerance)
-
 test_that("rasterize_bmtr() does not throw error with simple BMTR object", {
   local_quiet()
 
@@ -14,6 +8,7 @@ test_that("rasterize_bmtr() does not throw error with simple BMTR object", {
   bmtr_uw <- calc_bmtr(bf, weighted = FALSE)
 
   expect_no_error(raster_uw <- rasterize_bmtr(bmtr_uw, bf))
+  expect_no_error(terra::plot(raster_uw))
 })
 
 test_that("raster extent matches model extent", {
@@ -28,4 +23,31 @@ test_that("raster extent matches model extent", {
   raster_uw <- rasterize_bmtr(bmtr_uw, bf)
 
   expect_true(ext(bf) == ext(raster_uw))
+})
+
+test_that("number of layers in raster matches number of timesteps in model", {
+  local_quiet()
+
+  # Amewoo BirdFlow model
+  bf <- BirdFlowModels::amewoo
+
+  # Unweighted BMTR object
+  bmtr_uw <- calc_bmtr(bf, weighted = FALSE)
+
+  raster_uw <- rasterize_bmtr(bmtr_uw, bf)
+
+  expect_true(bf$metadata$n_timesteps == terra::nlyr(raster_uw))
+})
+
+test_that("rasterization works for weighted BMTR calculation", {
+  local_quiet()
+
+  # Amewoo BirdFlow model
+  bf <- BirdFlowModels::amewoo
+
+  # Weighted, non-Euclidean BMTR object
+  bmtr_sph <- calc_bmtr(bf, weighted = TRUE)
+
+  expect_no_error(raster_sph <- rasterize_bmtr(bmtr_sph, bf))
+  expect_no_error(terra::plot(raster_sph))
 })
